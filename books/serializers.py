a44c96bf1a6273df595d01d97abc0bd7926e33b8
Neeraj <neerajmayekar1608@gmail.com>
2025-11-10 23:07:40 +0530

    modified serializers.py.

--- books/serializers.py
+++ books/serializers.py
@@ -12,8 +12,11 @@ class AuthorSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class BookSerializer(serializers.ModelSerializer):
-    categories = CategorySerializer(many = True, read_only = True)
-    author = AuthorSerializer(read_only = True)
+    categories = serializers.PrimaryKeyRelatedField(
+        many = True,
+        queryset = Category.objects.all()
+    )
+    author = serializers.PrimaryKeyRelatedField(queryset = Author.objects.all())
     class Meta:
         model = Book
         fields = '__all__'
