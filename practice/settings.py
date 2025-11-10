58c91c5bc3d9f707a48069132d3eb79eb93bddc3
Neeraj <neerajmayekar1608@gmail.com>
2025-11-10 23:19:54 +0530

    added REST_FRAMEWORK in settings.py which includes AUTHENTICATION CLASSES and PERMISSION CLASSES.

--- practice/settings.py
+++ practice/settings.py
@@ -124,3 +124,13 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = 'login'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES':[
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES':[
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ],
+}
