67c372325c9c429945d6e4416434194d4c06681e
Neeraj <neerajmayekar1608@gmail.com>
2025-11-29 23:41:34 +0530

    added cors in middleware.

--- practice/settings.py
+++ practice/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -135,3 +136,5 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ],
 }
+
+CORS_ALLOW_ALL_ORIGINS = True
